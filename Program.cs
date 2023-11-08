cbf04f6dfcbfe25b10ce6a5c5e27a33257f33948
Hirbod Khatami <hirbod.khatami@gmail.com>
2023-11-08 17:20:25 +0000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace cicd_test;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! from version: v1.0.0");
+        }
+    }
